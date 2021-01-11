b6d5316c23761133c4f83288c12d0a3e556e2251
Weijiang Li <weijiangli@WeijiangdeMacBook-Pro.local>
2021-01-11 09:54:14 -0600

    added some sqlalchemy setup

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask, request, render_template
+
+
+
+# create a Flask application object and set the URI for the database to use
 app = Flask(__name__)
+app.config["SQLALCEHMY_DATABASE_URI"] = "sqlite:///example.sqlite3"
+
+# creating a database with dummy data
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def hello():
-    return "Hello World!"
-
+    return 'Hello World'
+    
 if __name__ == '__main__':
     app.run()
