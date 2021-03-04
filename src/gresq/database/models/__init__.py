5be6760c7a496c94a4975f0339e4c842952ba7aa
briancy02 <byoon2002@gmail.com>
2021-03-05 00:05:47 +0900

    author import added

--- src/gresq/database/models/__init__.py
+++ src/gresq/database/models/__init__.py
@@ -4,6 +4,7 @@ from gresq.database.models.environment_conditions import EnvironmentConditions
 from gresq.database.models.recipe import Recipe
 from gresq.database.models.preparation_step import PreparationStep
 from gresq.database.models.sample import Sample
+from gresq.database.models.author import Author
 # from gresq.database.models.sem_file import SemFile
 # from gresq.database.models.sem_analysis import SemAnalysis
 # from gresq.database.models.software import Software
