c8456779e880539e1a29b7b80f64988b2c63e561
jlee <lee0916jh@gmail.com>
2021-10-20 05:21:49 -0500

    made Database.base optional

--- server/db.py
+++ server/db.py
@@ -7,16 +7,16 @@ class DataBase:
     Class that is used to access database in webapp.
     """
 
-    def __init__(self, base):
+    def __init__(self, base=None):
         """
         Args:
             base (): declarative base
         """
         self.engine = None
         self.session = None
-        self.base = base
+        self.base = base if base else None
 
-    def init(self, db_url: str):
+    def init(self, db_url: str, enable_model_query=True):
         """
         Args:
             db_url (str): Database url to access the database.
@@ -26,4 +26,5 @@ class DataBase:
         self.session = scoped_session(sessionmaker(autocommit=False,
                                                    autoflush=False,
                                                    bind=self.engine))
-        self.base.query = self.session.query_property()
+        if enable_model_query and self.base:
+            self.base.query = self.session.query_property()
