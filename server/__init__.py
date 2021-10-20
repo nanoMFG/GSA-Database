85eec4c7c205a98ec2c9c0a1f80d34d0f51979ea
jlee <lee0916jh@gmail.com>
2021-10-20 04:50:34 -0500

    register api added

--- server/__init__.py
+++ server/__init__.py
@@ -1,9 +1,26 @@
+import os
 from flask import Flask
+from sqlalchemy import MetaData
 
+from src.grdb.database import Base as research_db_Base
+from .models import Base as webapp_db_Base
 from .db import DataBase
 
-research_db = DataBase()
-webapp_db = DataBase()
+meta = MetaData()
+research_db = DataBase(research_db_Base)
+webapp_db = DataBase(webapp_db_Base)
+
+
+def register_blueprints(app):
+    from .views.index import index
+    app.register_blueprint(index)
+    from .views.auth import auth
+    app.register_blueprint(auth)
+
+
+def create_schema(db: DataBase):
+    from .models import user
+    db.base.metadata.create_all(db.engine)
 
 
 def create_app():
@@ -12,9 +29,11 @@ def create_app():
     app.config.from_object('server.config.Config')
 
     research_db.init(app.config["DEV_DATABASE_URL"])
-    # TODO: webapp_db.init()
+    module_dir = os.path.dirname(os.path.abspath(__file__))
+    tempdb = os.path.join(module_dir, 'temp.db')
+    webapp_db.init('sqlite:///{}'.format(tempdb))  # TODO: Add another DB in AWS
 
-    from .views.index import index
-    app.register_blueprint(index)
+    register_blueprints(app)
+    create_schema(webapp_db)
 
     return app
