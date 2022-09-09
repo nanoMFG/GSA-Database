f4a637e4a598fc8007b6a6578e5b1d3361335972
lee0916jh <lee0916jh@gmail.com>
2022-09-08 21:05:49 -0700

    Move port and host to env file instead of hard coding

--- src/app.py
+++ src/app.py
@@ -1,4 +1,7 @@
 from grdb.server import app
+
 if __name__ == "__main__":
     application = app.create_app()
-    application.run(debug=True, host='0.0.0.0')
+    host = application.config['HOST']
+    port = application.config['PORT']
+    application.run(debug=True, host=host, port=port)
